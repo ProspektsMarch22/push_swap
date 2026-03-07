7dda29089ee6d6802145a23c5d42c8b0ce40f36b
Isaac Cezar Silva Barbosa <icezar-s@c1r2s1.42porto.com>
2026-03-07 17:54:44 +0000

    Fixes handler, has bugs on sort_5

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: icezar-s <icezar-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/03 14:57:50 by icezar-s          #+#    #+#             */
-/*   Updated: 2026/03/06 23:01:20 by icezar-s         ###   ########.fr       */
+/*   Updated: 2026/03/07 17:44:56 by icezar-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,12 @@ static void	handler(t_stack *a, t_stack *b)
 	size = a->q.size;
 	if (size == 0 || size == 1)
 		return ;
-	else if (size <= 3)
-		return (juggler_3(a));
+	if (size <= 3)
+		juggler_3(a);
 	else if (size <= 5)
-		return (juggler_5(a, b));
+		juggler_5(a, b);
 	else
-		return (butterfly_sort(a, b, size));
+		butterfly_sort(a, b, size);
 }
 
 int	main(int argc, char *argv[])
@@ -46,11 +46,9 @@ int	main(int argc, char *argv[])
 			args[i - 1] = ft_strdup(argv[i]);
 		parse_args(args, argc, a);
 	}
-	else
+	else if (argc == 2)
 		parse_arg(argv[1], a);
 	numbers_to_ranked(a);
 	handler(a, b);
-	free(a);
-	free(b);
 	return (0);
 }
